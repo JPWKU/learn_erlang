e2cc0dbfdc3c80f05c67430aa83c98b9dbb7f621
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2014-06-24 19:03:33 -0500

    added guards module

---
+++ guards.erl
@@ -0,0 +1,19 @@
+-module(guards).
+-export([old_enough/1,right_age/1,wrong_age/1]).
+
+old_enough(X) when X >= 16 -> 
+    true;
+old_enough(_) ->
+    false.
+
+right_age(X) when X >= 16, X =< 104 ->%% comma acts like andalso
+    true;
+right_age(_) ->
+    false.
+
+wrong_age(X) when X < 16; X > 104 ->%% semicolon acts like orelse
+    true;
+wrong_age(_) ->
+    false.
+
+
